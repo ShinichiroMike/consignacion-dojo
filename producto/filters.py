327db2c42489c9166a71d32281f1a1e1061f0b2f
mikeSernandez <shirowatla@gmail.com>
2017-12-19 23:11:13 +0100

    new style for profile, gt and lt filter for producto

--- producto/filters.py
+++ producto/filters.py
@@ -4,7 +4,10 @@ import django_filters
 class ProductoFilter(django_filters.FilterSet):
   nombre = django_filters.CharFilter(lookup_expr='icontains')
   color = django_filters.CharFilter(lookup_expr='icontains')
+  precio_base = django_filters.NumberFilter()
+  precio_base__gt = django_filters.NumberFilter(label="precio minimo", name='precio_base', lookup_expr='gt')
+  precio_base__lt = django_filters.NumberFilter(name='precio_base', lookup_expr='lt')
 
   class Meta:
       model = Producto
-      fields = ['nombre', 'color', 'precio_base', ]
+      fields = ['nombre', 'color', ]
