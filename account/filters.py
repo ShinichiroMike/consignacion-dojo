fc09df8958e4f76952ff624330a2f452f0c95020
mikeSernandez <shirowatla@gmail.com>
2017-12-21 20:19:17 +0100

    account filters improved

--- account/filters.py
+++ account/filters.py
@@ -4,6 +4,8 @@ import django_filters
 class UserFilter(django_filters.FilterSet):
   username = django_filters.CharFilter(lookup_expr='icontains')
   cif = django_filters.CharFilter(name='profile__cif', lookup_expr='icontains')
+  nombre = django_filters.CharFilter(name='profile__nombre', lookup_expr='icontains')
+  apellido = django_filters.CharFilter(name='profile__apellido', lookup_expr='icontains')
 
   class Meta:
       model = User
